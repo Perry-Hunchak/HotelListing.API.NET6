c115824d0ff3b2da064b5b15af9c77c8b5ed3460
Perry-Hunchak <phunchak@gmail.com>
2022-07-27 11:47:27 -0600

    Add project files.

---
+++ HotelListing/Data/HotelListingDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+	public class HotelListingDBContext : DbContext
+	{
+		public HotelListingDBContext(DbContextOptions options) : base(options)
+		{
+		
+		}
+
+	}
+}
